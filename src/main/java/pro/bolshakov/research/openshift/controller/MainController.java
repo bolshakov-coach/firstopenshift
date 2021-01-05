5f7352299e5f4f27652c8acda5cfe72c7be4a922
Aleksandr <mail.bolshakov@gmail.com>
2021-01-05 17:25:44 +0300

    Init

--- src/main/java/pro/bolshakov/research/openshift/controller/MainController.java
+++ src/main/java/pro/bolshakov/research/openshift/controller/MainController.java
@@ -1,14 +1,18 @@
 package pro.bolshakov.research.openshift.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
+    @Value("${test.var.message:not_found}")
+    private String testText;
+
     @GetMapping("")
     public String index(){
-        return "It is working";
+        return "It is working. Value from test.var.message -> " + testText;
     }
 
 }
